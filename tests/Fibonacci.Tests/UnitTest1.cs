6a2171a04a39d5e9a07051efe39678e406b5140a
Tristan <mytic12@hotmail.fr>
2021-10-13 18:29:35 +0200

    :sparkles: update database

--- tests/Fibonacci.Tests/UnitTest1.cs
+++ tests/Fibonacci.Tests/UnitTest1.cs
@@ -1,15 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using Xunit;
 
-namespace Fibonacci.Tests;
-
-public class UnitTest1
+namespace Fibonacci.Tests
 {
-    [Fact]        
-    public async Task Execute44ShouldRetrun701408733()       
-    {            
-        var result = await Compute.ExecuteAsync(new[] {"6"});          
-        Assert.Equal(701408733, result[0]);  
+
+    public class FibonacciUnitTest
+    {
+        [Fact]
+        public async Task Execute6ShouldRetrun8()
+        {
+            var builder = new DbContextOptionsBuilder<FibonacciDataContext>(); 
+            var dataBaseName = Guid.NewGuid().ToString();  
+            builder.UseInMemoryDatabase(dataBaseName);    
+            var options = builder.Options;  
+            var fibonacciDataContext = new FibonacciDataContext(options);  
+            await fibonacciDataContext.Database.EnsureCreatedAsync(); 
+
+            
+            var result =  await new Fibonacci.Compute(fibonacciDataContext).ExecuteAsync(new[] {"6"});
+            Assert.Equal(1, result.Count);
+            Assert.Equal(8, result[0]);
+        }
     }
-    
-    
 }
